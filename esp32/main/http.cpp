d2990fde8b676b427a9e01f98bc52808bb96a6c3
Xmow49 <dorian.benech007@gmail.com>
2023-05-10 16:52:35 +0200

    power managment + captive portal + jsp

--- esp32/main/http.cpp
+++ esp32/main/http.cpp
@@ -49,7 +49,8 @@ esp_err_t send_web_page(httpd_req_t *req)
     if (stat(url, &st))
     {
         ESP_LOGE(TAG, "%s not found", url);
-        return ESP_FAIL;
+        esp_err_t ret = httpd_resp_send_404(req);
+        return ret;
     }
 
     ESP_LOGI(TAG, "Allocating memory %ld bytes for %s", st.st_size, url);
@@ -103,16 +104,6 @@ esp_err_t send_web_page(httpd_req_t *req)
     free(data);
     return response;
 }
-esp_err_t get_req_handler(httpd_req_t *req)
-{
-    return send_web_page(req);
-}
-
-httpd_uri_t uri_get = {
-    .uri = "/",
-    .method = HTTP_GET,
-    .handler = get_req_handler,
-    .user_ctx = NULL};
 
 // HTTP Error (404) Handler - Redirects all requests to the root page
 esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
@@ -128,6 +119,42 @@ esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
     return ESP_OK;
 }
 
+esp_err_t get_req_handler(httpd_req_t *req)
+{
+    return send_web_page(req);
+}
+
+esp_err_t get_req_204_handler(httpd_req_t *req)
+{
+    // Set status
+    httpd_resp_set_status(req, "302 Temporary Redirect");
+    // Redirect to the "/" root directory
+    httpd_resp_set_hdr(req, "Location", "/");
+    // iOS requires content in the response to detect a captive portal, simply redirecting is not sufficient.
+    httpd_resp_send(req, "Redirect to the captive portal", HTTPD_RESP_USE_STRLEN);
+
+    ESP_LOGI(TAG, "Redirecting to root");
+    return ESP_OK;
+}
+
+httpd_uri_t uri_get = {
+    .uri = "/*",
+    .method = HTTP_GET,
+    .handler = get_req_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t uri_204 = {
+    .uri = "/gen_204",
+    .method = HTTP_GET,
+    .handler = get_req_204_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t uri_2044 = {
+    .uri = "/generate_204",
+    .method = HTTP_GET,
+    .handler = get_req_204_handler,
+    .user_ctx = NULL};
+
 httpd_handle_t setup_server(void)
 {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
@@ -139,6 +166,8 @@ httpd_handle_t setup_server(void)
     if (httpd_start(&server, &config) == ESP_OK)
     {
         httpd_register_err_handler(server, HTTPD_404_NOT_FOUND, http_404_error_handler);
+        httpd_register_uri_handler(server, &uri_204);
+        httpd_register_uri_handler(server, &uri_2044);
         httpd_register_uri_handler(server, &uri_get);
         // httpd_register_uri_handler(server, &uri_on);
         // httpd_register_uri_handler(server, &uri_off);
