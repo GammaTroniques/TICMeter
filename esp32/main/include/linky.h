6579f707ae5e6c9cd2a37a64f71704ef4cbe422e
Xmow49 <dorian.benech007@gmail.com>
2023-08-13 22:04:54 +0200

    somes linky fix (triphase)

--- esp32/main/include/linky.h
+++ esp32/main/include/linky.h
@@ -22,7 +22,7 @@
 #define RX_BUF_SIZE     1024 // The size of the UART buffer
 #define BUFFER_SIZE     1024 // The size of the UART buffer
 #define FRAME_SIZE      500   // The size of one frame buffer
-#define GROUP_COUNT     15
+#define GROUP_COUNT     50
 
 #define LINKY_TAG "Linky"
 
@@ -30,50 +30,50 @@ typedef struct
 {
     //  Variables                   Taille      Unité       Description
     //-----------------------------------------------------------------------
-    char ADCO[13]       = {0}; //    12                     Adresse du compteur 
-    char OPTARIF[5]     = {0}; //     4                     Option tarifaire choisie
-    uint32_t ISOUSC     = 0;   //     2         A           Intensité souscrite 
+    char ADCO[13]       = {0};          //    12                     Adresse du compteur 
+    char OPTARIF[5]     = {0};          //     4                     Option tarifaire choisie
+    uint16_t ISOUSC     = UINT16_MAX;   //     2         A           Intensité souscrite 
 
-    uint32_t BASE       = 0;   //     9         Wh          Index option Base 
+    uint64_t BASE       = UINT64_MAX;   //     9         Wh          Index option Base 
     //----------------------Index option Heures Creuses ----------------------
-    uint32_t HCHC       = 0;   //     9         Wh          Index option Heures Creuses: Heures Creuses
-    uint32_t HCHP       = 0;   //     9         Wh          Index option Heures Creuses: Heures Pleines
+    uint64_t HCHC       = UINT64_MAX;   //     9         Wh          Index option Heures Creuses: Heures Creuses
+    uint64_t HCHP       = UINT64_MAX;   //     9         Wh          Index option Heures Creuses: Heures Pleines
     
     //---------------------- Index option EJP  (Effacement des Jours de Pointe) --> 22 jours par an prix du kWh plus cher
-    uint32_t EJPHN      = 0;   //     9         Wh          Heures Normales
-    uint32_t EJPHPM     = 0;   //     9         Wh          Heures de Pointe Mobile
-    uint32_t PEJP       = 0;   //     2         minutes     Préavis Début EJP (30 min)
+    uint64_t EJPHN      = UINT64_MAX;   //     9         Wh          Heures Normales
+    uint64_t EJPHPM     = UINT64_MAX;   //     9         Wh          Heures de Pointe Mobile
+    uint64_t PEJP       = UINT64_MAX;   //     2         minutes     Préavis Début EJP (30 min)
 
     //---------------------- Index option Tempo ----------------------
-    uint32_t BBRHCJB    = 0;   //     9         Wh          Heures Creuses Jours Bleus
-    uint32_t BBRHPJB    = 0;   //     9         Wh          Heures Pleines Jours Bleus
-    uint32_t BBRHCJW    = 0;   //     9         Wh          Heures Creuses Jours Blancs
-    uint32_t BBRHPJW    = 0;   //     9         Wh          Heures Pleines Jours Blancs
-    uint32_t BBRHCJR    = 0;   //     9         Wh          Heures Creuses Jours Rouges
-    uint32_t BBRHPJR    = 0;   //     9         Wh          Heures Pleines Jours Rouges
-    
-    char PTEC[5]        = {0}; //     4                     Période Tarifaire en cours: TH.. Heures Creuses, HP.. Heures Pleines, HC.. Heures Creuses, HN.. Heures Normales, PM.. Heures de Pointe Mobile, HCJB.. Heures Creuses Jours Bleus, HPJB.. Heures Pleines Jours Bleus, HCJW.. Heures Creuses Jours Blancs, HPJW.. Heures Pleines Jours Blancs, HCJR.. Heures Creuses Jours Rouges, HPJR.. Heures Pleines Jours Rouges
-    char DEMAIN[5]      = {0}; //     4                     Couleur du lendemain 
-    
-    uint32_t IINST      = 0;   //     3         A           Intensité Instantanée
-    uint32_t IINST1     = 0;   //     3         A           Intensité Instantanée Phase 1
-    uint32_t IINST2     = 0;   //     3         A           Intensité Instantanée Phase 2
-    uint32_t IINST3     = 0;   //     3         A           Intensité Instantanée Phase 3
-    uint32_t IMAX       = 0;   //     3         A           Intensité maximale appelée
-    uint32_t IMAX1      = 0;   //     3         A           Intensité maximale appelée Phase 1
-    uint32_t IMAX2      = 0;   //     3         A           Intensité maximale appelée Phase 2
-    uint32_t IMAX3      = 0;   //     3         A           Intensité maximale appelée Phase 3
-    uint32_t ADPS       = 0;   //     3         A           Avertissement de Dépassement de Puissance Souscrite
-    uint32_t ADIR1      = 0;   //     3         A           Avertissement de Dépassement d'intensité de réglage Phase 1
-    uint32_t ADIR2      = 0;   //     3         A           Avertissement de Dépassement d'intensité de réglage Phase 2
-    uint32_t ADIR3      = 0;   //     3         A           Avertissement de Dépassement d'intensité de réglage Phase 3
-    
-    uint32_t PAPP       = 0;   //     5         VA          Puissance apparente
-    uint32_t PMAX       = 0;   //     5         W           Puissance maximale triphasée atteinte 
-    uint32_t PPOT       = 0;   //     2                     Présence des potentiels??????????????????????????????
+    uint64_t BBRHCJB    = UINT64_MAX;   //     9         Wh          Heures Creuses Jours Bleus
+    uint64_t BBRHPJB    = UINT64_MAX;   //     9         Wh          Heures Pleines Jours Bleus
+    uint64_t BBRHCJW    = UINT64_MAX;   //     9         Wh          Heures Creuses Jours Blancs
+    uint64_t BBRHPJW    = UINT64_MAX;   //     9         Wh          Heures Pleines Jours Blancs
+    uint64_t BBRHCJR    = UINT64_MAX;   //     9         Wh          Heures Creuses Jours Rouges
+    uint64_t BBRHPJR    = UINT64_MAX;   //     9         Wh          Heures Pleines Jours Rouges
 
-    char HHPHC[4]       = {0}; //     1                     Horaire Heures Pleines Heures Creuses
-    char MOTDETAT[7]    = {0}; //     6                     Mot d'état du compteur                            
+    char PTEC[5]        = {0};          //     4                     Période Tarifaire en cours: TH.. Heures Creuses, HP.. Heures Pleines, HC.. Heures Creuses, HN.. Heures Normales, PM.. Heures de Pointe Mobile, HCJB.. Heures Creuses Jours Bleus, HPJB.. Heures Pleines Jours Bleus, HCJW.. Heures Creuses Jours Blancs, HPJW.. Heures Pleines Jours Blancs, HCJR.. Heures Creuses Jours Rouges, HPJR.. Heures Pleines Jours Rouges
+    char DEMAIN[5]      = {0};          //     4                     Couleur du lendemain 
+
+    uint16_t IINST      = UINT16_MAX;   //     3         A           Intensité Instantanée
+    uint16_t IINST1     = UINT16_MAX;   //     3         A           Intensité Instantanée Phase 1
+    uint16_t IINST2     = UINT16_MAX;   //     3         A           Intensité Instantanée Phase 2
+    uint16_t IINST3     = UINT16_MAX;   //     3         A           Intensité Instantanée Phase 3
+    uint16_t IMAX       = UINT16_MAX;   //     3         A           Intensité maximale appelée
+    uint16_t IMAX1      = UINT16_MAX;   //     3         A           Intensité maximale appelée Phase 1
+    uint16_t IMAX2      = UINT16_MAX;   //     3         A           Intensité maximale appelée Phase 2
+    uint16_t IMAX3      = UINT16_MAX;   //     3         A           Intensité maximale appelée Phase 3
+    uint16_t ADPS       = UINT16_MAX;   //     3         A           Avertissement de Dépassement de Puissance Souscrite
+    uint16_t ADIR1      = UINT16_MAX;   //     3         A           Avertissement de Dépassement d'intensité de réglage Phase 1
+    uint16_t ADIR2      = UINT16_MAX;   //     3         A           Avertissement de Dépassement d'intensité de réglage Phase 2
+    uint16_t ADIR3      = UINT16_MAX;   //     3         A           Avertissement de Dépassement d'intensité de réglage Phase 3
+
+    uint32_t PAPP       = UINT32_MAX;   //     5         VA          Puissance apparente
+    uint32_t PMAX       = UINT32_MAX;   //     5         W           Puissance maximale triphasée atteinte 
+    uint32_t PPOT       = UINT32_MAX;   //     2                     Présence des potentiels??????????????????????????????
+
+    char HHPHC[4]       = {0};          //     1                     Horaire Heures Pleines Heures Creuses
+    char MOTDETAT[7]    = {0};          //     6                     Mot d'état du compteur                            
     
     time_t timestamp    = 0;
 } LinkyData;
@@ -82,7 +82,7 @@ typedef struct
 #define TYPE_UINT8  1
 #define TYPE_UINT16 2
 #define TYPE_UINT32 3
-#define TYPE_UINT48 4
+#define TYPE_UINT64 4
 
 struct LinkyGroup
 {
@@ -119,6 +119,9 @@ private:
     char UARTRX = 0;                // The RX pin of the linky
     char UARTTX = 0;                // The TX pin of the linky
     uint8_t GROUP_SEPARATOR = 0x20; // The group separator character (changes depending on the mode) (0x20 in historique mode, 0x09 in standard mode)
+
+    char *frame = NULL;     // The frame to send to the linky
+    uint16_t frameSize = 0; // The size of the frame
 
     void read();                                        // Read the UART buffer
     char decode();                                      // Decode the frame
