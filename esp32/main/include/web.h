1c242dfe404b6d5bb7b48b339b11701ca0a5353f
Xmow49 <dorian.benech007@gmail.com>
2023-09-02 18:31:35 +0200

    adc calibration + start of shell struct

--- esp32/main/include/web.h
+++ esp32/main/include/web.h
@@ -4,6 +4,14 @@
 #include "linky.h"
 #include "config.h"
 
+/**
+ * @brief prepare json data to send to server
+ *
+ * @param data  the Array of data to send
+ * @param dataIndex  the index of the data to send
+ * @param json  the json destination
+ * @param jsonSize the size of the json destination
+ */
 void preapareJsonData(LinkyData *data, char dataIndex, char *json, unsigned int jsonSize);
 
 #endif // !WEB_H
