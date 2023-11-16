cf72644fbd6b62af409cfd3f8d3da91b5d4efbff
Dorian Benech <dorian.benech007@gmail.com>
2023-11-16 22:56:36 +0100

    script

--- firmware/main/version.c
+++ firmware/main/version.c
@@ -1,4 +1,4 @@
-const char* GIT_REV="2b9eacb";
+const char* GIT_REV="d244065";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="dev";
-const char* BUILD_TIME="2023-11-16 22:06:06";
+const char* BUILD_TIME="2023-11-16 22:29:24";
