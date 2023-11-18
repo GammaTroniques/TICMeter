718bc9f90c8170da5b7d84466cb5b2c87874893d
Dorian Benech <dorian.benech007@gmail.com>
2023-11-18 11:38:34 +0100

    ota

--- firmware/main/version.c
+++ firmware/main/version.c
@@ -1,4 +1,4 @@
-const char* GIT_REV="37dac98";
+const char* GIT_REV="39857f6";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="dev";
-const char* BUILD_TIME="2023-11-18 10:35:12";
+const char* BUILD_TIME="2023-11-18 11:38:13";
