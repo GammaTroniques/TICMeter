cd328410a01998c2a0a1c41d7bc833a34633ca6d
Dorian Benech <dorian.benech007@gmail.com>
2023-11-18 12:11:37 +0100

    fix ota

--- firmware/main/version.c
+++ firmware/main/version.c
@@ -1,4 +1,4 @@
-const char* GIT_REV="1079b63";
-const char* GIT_TAG="";
+const char* GIT_REV="416e046";
+const char* GIT_TAG="V3.0.3";
 const char* GIT_BRANCH="dev";
-const char* BUILD_TIME="2023-11-18 11:40:45";
+const char* BUILD_TIME="2023-11-18 12:04:48";
