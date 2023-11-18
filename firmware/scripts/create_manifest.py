3b39df10dd0242f5412a801f2e25fe5fa82afb88
Dorian Benech <dorian.benech007@gmail.com>
2023-11-18 11:38:34 +0100

    ota

--- firmware/scripts/create_manifest.py
+++ firmware/scripts/create_manifest.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import re
+import subprocess
 
 csv_file = "../partitions.csv"
 json_file = "../build/manifest.json"
@@ -29,23 +30,27 @@ with open(csv_file, mode='r') as file:
         if row[0] in toFill:
             offset.append(int(row[3], 16))
             print(f"{row[0]}: {row[3]}")
-        
+
+# git describe --tags
+version = subprocess.check_output(["git", "describe", "--tags"]).decode("utf-8").strip()
+
 # Construire la structure JSON avec les nouvelles valeurs d'offset
 output_data = {
-    "name": "ESP Linky TIC",
-    "version": "2.0",
+    "name": "TICMeter",
+    "version": version,
     "home_assistant_domain": "esphome",
     "funding_url": "https://esphome.io/guides/supporters.html",
     "new_install_prompt_erase": True,
     "builds": [
         {
             "chipFamily": "ESP32-C6",
+            "target": "esp32c6",
             "parts": [
                 {"path": fileURL + "bootloader.bin", "offset": 0},
                 {"path": fileURL + "partition-table.bin", "offset": 32768},
                 {"path": fileURL + "otadata_initial.bin", "offset": offset[0]},
-                {"path": fileURL + "storage.bin", "offset": offset[1]},
-                {"path": fileURL + "TICMeter.bin", "offset": offset[2]}
+                {"path": fileURL + "storage.bin", "offset": offset[1], "type": "storage"},
+                {"path": fileURL + "TICMeter.bin", "offset": offset[2], "type": "app"},
             ]
         }
     ]
