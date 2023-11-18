d4471f4c2fc311e6be1935964644488395b033d3
Dorian Benech <dorian.benech007@gmail.com>
2023-11-18 18:41:23 +0100

    fix refresh rate + optimisation

--- firmware/components/tuya-iot-link-sdk/tuya-connect-kit-for-mqtt-embedded-c/include/tuya_log.h
+++ firmware/components/tuya-iot-link-sdk/tuya-connect-kit-for-mqtt-embedded-c/include/tuya_log.h
@@ -3,16 +3,28 @@
 
 #include "log.h"
 
+#ifdef ENABLE_LOGS
 #define TY_LOGE log_error
 #define TY_LOGW log_warn
 #define TY_LOGI log_info
 #define TY_LOGD log_debug
 #define TY_LOGV log_trace
-
-#define PR_ERR   log_error
-#define PR_WARN  log_warn
-#define PR_INFO  log_info
+#define PR_ERR log_error
+#define PR_WARN log_warn
+#define PR_INFO log_info
 #define PR_DEBUG log_debug
 #define PR_TRACE log_trace
+#else
+#define TY_LOGE(...) ((void)0)
+#define TY_LOGW(...) ((void)0)
+#define TY_LOGI(...) ((void)0)
+#define TY_LOGD(...) ((void)0)
+#define TY_LOGV(...) ((void)0)
+#define PR_ERR(...) ((void)0)
+#define PR_WARN(...) ((void)0)
+#define PR_INFO(...) ((void)0)
+#define PR_DEBUG(...) ((void)0)
+#define PR_TRACE(...) ((void)0)
+#endif
 
 #endif
--- firmware/main/version.c
+++ firmware/main/version.c
@@ -1,4 +1,4 @@
-const char* GIT_REV="e0760d3";
+const char* GIT_REV="84cbaaa";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="dev";
-const char* BUILD_TIME="2023-11-18 12:33:58";
+const char* BUILD_TIME="2023-11-18 18:35:29";
