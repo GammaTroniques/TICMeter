52259d5ea1b8076f65cd5d8e08d22ce0f70fa0ca
Dorian Benech <47485034+xmow49@users.noreply.github.com>
2023-11-18 18:47:21 +0100

    V3.0.1 (#3)
    OTA check later
    refresh rate security

--- firmware/main/version.c
+++ firmware/main/version.c
@@ -1,4 +1,4 @@
-const char* GIT_REV="e0760d3";
+const char* GIT_REV="7744c5b";
 const char* GIT_TAG="";
 const char* GIT_BRANCH="dev";
-const char* BUILD_TIME="2023-11-18 12:33:58";
+const char* BUILD_TIME="2023-11-18 18:44:34";
