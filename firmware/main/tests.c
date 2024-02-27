7b202398bf42bc878ea6297c92256716c4cb3f24
Dorian Benech <dorian.benech007@gmail.com>
2024-02-27 21:25:58 +0100

    zigbee test std

--- firmware/main/tests.c
+++ firmware/main/tests.c
@@ -24,6 +24,7 @@
 #include "driver/uart.h"
 #include "linky.h"
 #include "common.h"
+#include "wifi.h"
 #include "main.h"
 /*==============================================================================
  Local Define
@@ -145,5 +146,88 @@ static esp_err_t test_linky_hist(void *ptr)
 
 static esp_err_t test_linky_std(void *ptr)
 {
+    linky_data_std data = {
+        .ADSC = "123456789012",
+        .VTIC = "2",
+        .DATE = {.value = 0, .timestamp = wifi_get_timestamp()},
+        .NGTF = "TEMPO",
+        .LTARF = "HP  BLEU",
+        .EAST = 50019226,
+        .EASF01 = 022235340,
+        .EASF02 = 26587270,
+        .EASF03 = 26587270,
+        .EASF04 = 494614,
+        .EASF05 = 115045,
+        .EASF06 = 161261,
+        .EASF07 = 7,
+        .EASF08 = 8,
+        .EASF09 = 9,
+        .EASF10 = 10,
+        .EASD01 = 11,
+        .EASD02 = 12,
+        .EASD03 = 13,
+        .EASD04 = 14,
+        .EAIT = 115,
+        .IRMS1 = 15,
+        .IRMS2 = 16,
+        .IRMS3 = 17,
+        .URMS1 = 18,
+        .URMS2 = 19,
+        .URMS3 = 20,
+        .PREF = 21,
+        .PCOUP = 22,
+        .SINSTS = 23,
+        .SINSTS1 = 24,
+        .SINSTS2 = 25,
+        .SINSTS3 = 26,
+        .SMAXSN = {.value = 27, .timestamp = wifi_get_timestamp()},
+        .SMAXSN1 = {.value = 28, .timestamp = wifi_get_timestamp()},
+        .SMAXSN2 = {.value = 29, .timestamp = wifi_get_timestamp()},
+        .SMAXSN3 = {.value = 30, .timestamp = wifi_get_timestamp()},
+
+        .SMAXSN_1 = {.value = 31, .timestamp = wifi_get_timestamp()},
+        .SMAXSN1_1 = {.value = 32, .timestamp = wifi_get_timestamp()},
+        .SMAXSN2_1 = {.value = 33, .timestamp = wifi_get_timestamp()},
+        .SMAXSN3_1 = {.value = 34, .timestamp = wifi_get_timestamp()},
+
+        .SINSTI = 35,
+
+        .SMAXIN = {.value = 36, .timestamp = wifi_get_timestamp()},
+        .SMAXIN_1 = {.value = 37, .timestamp = wifi_get_timestamp()},
+
+        .CCASN = {.value = 38, .timestamp = wifi_get_timestamp()},
+        .CCASN_1 = {.value = 39, .timestamp = wifi_get_timestamp()},
+        .CCAIN = {.value = 40, .timestamp = wifi_get_timestamp()},
+        .CCAIN_1 = {.value = 41, .timestamp = wifi_get_timestamp()},
+
+        .UMOY1 = {.value = 42, .timestamp = wifi_get_timestamp()},
+        .UMOY2 = {.value = 43, .timestamp = wifi_get_timestamp()},
+        .UMOY3 = {.value = 44, .timestamp = wifi_get_timestamp()},
+
+        .STGE = "000000",
+
+        .DPM1 = {.value = 45, .timestamp = wifi_get_timestamp()},
+        .FPM1 = {.value = 46, .timestamp = wifi_get_timestamp()},
+        .DPM2 = {.value = 47, .timestamp = wifi_get_timestamp()},
+        .FPM2 = {.value = 48, .timestamp = wifi_get_timestamp()},
+        .DPM3 = {.value = 49, .timestamp = wifi_get_timestamp()},
+        .FPM3 = {.value = 50, .timestamp = wifi_get_timestamp()},
+
+        .MSG1 = "test",
+        .MSG2 = "un test",
+        .PRM = "25555555926695",
+        .RELAIS = "00000",
+        .NTARF = "02",
+        .NJOURF = "00",
+        .NJOURF_1 = "00",
+        .PJOURF_1 = "00",
+        .PPOINTE = "000000",
+    };
+
+    linky_set_mode(MODE_STD);
+    linky_data.std = data;
+    linky_print();
+    main_send_data();
+
     return ESP_OK;
 }
